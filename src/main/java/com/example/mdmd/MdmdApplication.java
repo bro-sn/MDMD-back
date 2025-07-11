f5fe40de766febb442570ea00d446d2cf138d368
bro-sn <sinyada3@gmail.com>
2025-07-11 13:30:37 +0900

    기본 툴

---
+++ src/main/java/com/example/mdmd/MdmdApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmdApplication.class, args);
+	}
+
+}
