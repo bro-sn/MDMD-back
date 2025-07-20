fb211b3a77eba9c0b7bb3e2d9cad27c8af612989
rmia4 <psd020517@naver.com>
2025-07-21 02:53:04 +0900

    @ResponseBody + 객체 리턴

--- src/main/java/com/example/mdmd/controllers/sdController.java
+++ src/main/java/com/example/mdmd/controllers/sdController.java
@@ -1,7 +1,10 @@
 package com.example.mdmd.controllers;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class sdController {
@@ -14,6 +17,22 @@ public class sdController {
     public String sd2(){
         return "sd2";
 
+    }
+    @Setter
+    @Getter
+    static class Book{
+        private String name;
+        private int num;
+
+    }
+
+    @GetMapping("sd-obj")
+    @ResponseBody
+    public Book sdObj(){
+        Book b = new Book();
+        b.setName("aabb");
+        b.setNum(1234);
+        return b;
     }
 
 
